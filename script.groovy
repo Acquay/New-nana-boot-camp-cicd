fff9ef70e8578c442187a110fa860a16a5d695c7
acquay <acquay707@mail.com>
2025-01-03 07:26:26 -0600

    jenkinsfile update

--- script.groovy
+++ script.groovy
@@ -4,12 +4,14 @@ def buildJar() {
 }
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
+    echo "Building the Docker image..."
+            withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                sh """
+                    docker build -t ztech101/nana-docker-image:jma-${env.BUILD_NUMBER} .
+                    echo \$PASS | docker login -u \$USER --password-stdin
+                    docker push ztech101/nana-docker-image:jma-${env.BUILD_NUMBER}
+                """
+            }
 }
 
 def deployApp() {
